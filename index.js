a854d5e4c1af8cb136131a1b3603b7bbd0954935
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-01 00:08:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector);
+  return element;
+}
+
+function nestedTarget() {
+  var element = document.querySelector('#nested .target');
+  return element;
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = ((parseInt(lis[i])+1).toString());
+  }
+}
+
+
+function deepestChild() {
+  var current = document.querySelector('#grand-node');
+  var next = [];
+  while(current) {
+    if(current.querySelector('div')) {
+      next.push(current.querySelector('div'));
+      current = next.shift();
+    }
+    else {
+      return current;
+    }
+    
+  }
+}
